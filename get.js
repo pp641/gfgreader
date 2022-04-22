54ac765cc29bdc10373f7c63dbe9fea8ec88af2a
pp641 <prajjwalpandey641@gmail.com>
2022-04-22 12:22:23 +0530

    build changes

--- get.js
+++ get.js
@@ -3,20 +3,18 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
-const dotenv = require('dotenv');
 app.use(
   cors({
     origin: "*",
   })
 );
 
-dotenv.config({path: './config.env'})
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const router = require("./Routes/routes");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-mongoose.connect(process.env.databaseLink);
+mongoose.connect(process.env.MONGOURI);
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function callback() {
