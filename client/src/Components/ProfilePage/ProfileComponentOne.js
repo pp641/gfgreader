77ad5e4f669e54a9ec3667301f330bad4d10f788
pp641 <prajjwalpandey641@gmail.com>
2022-04-22 19:37:47 +0530

    added accordion

--- client/src/Components/ProfilePage/ProfileComponentOne.js
+++ client/src/Components/ProfilePage/ProfileComponentOne.js
@@ -1,33 +1,42 @@
 import { Button, Typography } from "@mui/material";
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 const ProfileComponentOne = (props) => {
   const navigate = useNavigate();
+  const records = useSelector((state) => state);
   const [profilePageDetails, setProfilePageDetails] = React.useState(
     JSON.parse(localStorage.getItem("payload"))
   );
   return (
-    <div className='container'>
+    <div className="container">
       <div className="profile_box p-5 shadow rounded border">
         <div className="row p-3">
-          <Typography variant="h3" className="h3"> Welcome to the Profile </Typography>
+          <Typography variant="h3" className="h3">
+            {" "}
+            Welcome to the Profile{" "}
+          </Typography>
         </div>
 
         <div className="row p-3">
           <Typography variant="h5">
-              User Name : {profilePageDetails.name}
+            User Name : {profilePageDetails.name}
           </Typography>
         </div>
 
         <div className="row p-1">
-          <Typography variant="h5">Email : {profilePageDetails.email}</Typography>
+          <Typography variant="h5">
+            Email : {profilePageDetails.email}
+          </Typography>
         </div>
-        
+
+        <div className="row p-1">
+          <Typography variant="h5">
+            Posts Read :{" "}
+            {records.ArticleReducers?.allSavedRecordsUserWise.length}
+          </Typography>
+        </div>
       </div>
-      <Button         variant  = "contained"
- onClick  = {()=>{
-        navigate("/pagination")
-      }}> go to all Posts</Button>
     </div>
   );
 };
